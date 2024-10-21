d4cd05cfc8ced8e7d15c0128274e9b3f0d3abc1b
nebo1991 <nebojsa.kovacevic1991@gmail.com>
2024-10-21 14:53:19 +0200

    feat: add navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+
+    <Navbar />
+    </>
   )
 }
 
