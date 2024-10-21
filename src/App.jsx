2f41e00bef68ed63e209080df8aeeb4c4f2b5dbb
nebo1991 <nebojsa.kovacevic1991@gmail.com>
2024-10-21 16:11:59 +0200

    feat: add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
 
     <Navbar />
+    <HeroSection />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <img href="#" alt="books-logo" src={navBarLogo} className='w-[100px] mx-8 my-4'/>
         <div className="flex gap-20 my-10 mx-4">
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900 my-3">
+            <a href="#" className="text-sm font-semibold leading-6 text-gray-900 my-3 hover:text-purple-400">
               About <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
