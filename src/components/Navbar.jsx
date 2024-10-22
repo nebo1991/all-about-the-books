e7bc23134122ce9f713636d25c6293bbd7cab2a2
nebo1991 <nebojsa.kovacevic1991@gmail.com>
2024-10-22 11:36:40 +0200

    feat: add about us page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,28 @@
-import navBarLogo from '../assets/book-logo.svg'
+import navBarLogo from "../assets/book-logo.svg";
 
 const Navbar = () => {
-    return(
-        <>
-        <div className="flex justify-between">
-        <img href="#" alt="books-logo" src={navBarLogo} className='w-[100px] mx-8 my-4'/>
+  return (
+    <>
+      <div className="flex justify-between">
+        <img
+          href="#"
+          alt="books-logo"
+          src={navBarLogo}
+          className="w-[100px] mx-8 my-4"
+        />
         <div className="flex gap-20 my-10 mx-4">
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900 my-3 hover:text-purple-400">
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <a
+              href="/AboutUs"
+              className="text-sm font-semibold leading-6 text-gray-900 my-3 hover:text-purple-400"
+            >
               About <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
-        
-    
         </div>
-        </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
