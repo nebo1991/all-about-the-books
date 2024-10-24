319d8ad8a7cae93050cfe2bc5310ea6a2280bd4f
nebo1991 <nebojsa.kovacevic1991@gmail.com>
2024-10-24 16:44:35 +0200

    fix: link to about us page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,12 +14,11 @@ const Navbar = () => {
         </Link>
         <div className="flex gap-20 my-10 mx-4">
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a
-              href="/AboutUs"
-              className="text-sm font-semibold leading-6 text-gray-900 my-3 hover:text-purple-400 t"
-            >
-              About <span aria-hidden="true">&rarr;</span>
-            </a>
+            <Link to="/AboutUs">
+              <p className="text-sm font-semibold leading-6 text-gray-900 my-3 hover:text-purple-400 t">
+                About <span aria-hidden="true">&rarr;</span>
+              </p>
+            </Link>
           </div>
         </div>
       </div>
