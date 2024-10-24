406ad6916c8419fd069a3a7cba1f703c9f320ccb
nebo1991 <nebojsa.kovacevic1991@gmail.com>
2024-10-24 15:33:19 +0200

    fix: readme update and styling

--- src/pages/AddNewBookPage.jsx
+++ src/pages/AddNewBookPage.jsx
@@ -61,7 +61,7 @@ const AddNewBookPage = () => {
       <div className="space-y-12 mt-20">
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900 flex justify-center items-center">
-            Book Information
+            Add new book
           </h2>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
