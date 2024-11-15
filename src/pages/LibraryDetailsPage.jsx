46a82530024acc4640628b09a1487d3a54e80f39
nebo1991 <nebojsa.kovacevic1991@gmail.com>
2024-11-15 11:42:24 +0100

    feat: remove book from library

--- src/pages/LibraryDetailsPage.jsx
+++ src/pages/LibraryDetailsPage.jsx
@@ -4,11 +4,12 @@ import { Link, useParams } from "react-router-dom";
 import { useAuthContext } from "../context/AuthContext";
 
 const LibraryDetailsPage = () => {
-  const { idLibrary } = useParams(); // Get the idLibrary from the URL parameters
+  const { idLibrary } = useParams();
   const { isLoggedIn } = useAuthContext();
   const [library, setLibrary] = useState(null);
   const token = localStorage.getItem("authToken");
 
+  // Fetch library details
   const fetchLibrary = async (idLibrary) => {
     try {
       const response = await axios.get(
@@ -19,18 +20,34 @@ const LibraryDetailsPage = () => {
           },
         }
       );
-      console.log("response: ", response.data);
       setLibrary(response.data);
     } catch (error) {
       console.error("Error fetching library:", error);
     }
   };
 
+  const handleRemoveBook = async (bookId) => {
+    try {
+      const response = await axios.put(
+        `http://localhost:3000/libraries/${idLibrary}/remove-book`,
+        { bookId },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setLibrary(response.data);
+    } catch (error) {
+      console.error("Error removing book from library:", error);
+    }
+  };
+
   useEffect(() => {
     if (idLibrary) {
-      fetchLibrary(idLibrary); // Pass idLibrary when calling the function
+      fetchLibrary(idLibrary);
     }
-  }, [idLibrary]); // Add idLibrary as a dependency
+  }, [idLibrary]);
 
   if (!isLoggedIn) {
     return <p>You need to log in to view this page.</p>;
@@ -47,36 +64,32 @@ const LibraryDetailsPage = () => {
         <h2 className="text-2xl font-semibold text-black">{library.name}</h2>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        {library.books.map((book) => {
-          return (
-            <Link key={book.id} to={`/books/${book._id}`}>
-              <div className="group relative">
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                  <img
-                    alt="book-cover"
-                    src={book.image}
-                    className="h-full w-full object-fill lg:h-full lg:w-full"
-                  />
-                </div>
-                <div className="mt-4 flex justify-between">
-                  <div>
-                    <h3 className="text-sm text-gray-700">
-                      <a>
-                        <span aria-hidden="true" className="absolute inset-0" />
-                        {book.title}
-                      </a>
-                    </h3>
-                    <div className="flex items-baseline gap-20">
-                      <p className="mt-1 text-sm text-gray-500">
-                        {book.author}
-                      </p>
-                    </div>
-                  </div>
-                </div>
+        {library.books.map((book) => (
+          <div key={book._id} className="group relative">
+            <Link to={`/books/${book._id}`}>
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img
+                  alt="book-cover"
+                  src={book.image}
+                  className="h-full w-full object-fill lg:h-full lg:w-full"
+                />
               </div>
             </Link>
-          );
-        })}
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">{book.title}</h3>
+                <p className="mt-1 text-sm text-gray-500">{book.author}</p>
+              </div>
+              <button
+                onClick={() => handleRemoveBook(book._id)}
+                className="text-red-500 hover:text-red-700"
+                title="Remove Book"
+              >
+                💔
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
